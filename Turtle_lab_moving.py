049f1a691587e7be52ce503cda02943fbd9d1ad8
leocho12 <hjc021212@gmail.com>
2025-09-09 12:05:06 +0900

    DRILL #1: 거북이를 “WASD”키로 조종하기 생성
    Drill #1 제출 - 2022184039 조형진

---
+++ Turtle_lab_moving.py
@@ -0,0 +1,35 @@
+import turtle
+import random
+
+def up():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def down():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def right():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def left():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart():
+    turtle.reset()
+
+turtle.shape('turtle')
+
+turtle.onkey(up,'w')
+turtle.onkey(down,'s')
+turtle.onkey(right,'d')
+turtle.onkey(left,'a')
+turtle.onkey(restart,'Escape')
+turtle.listen()
+turtle.mainloop()
